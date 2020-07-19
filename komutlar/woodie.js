910ccab384f14d488ce25b91286e95167c495045
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2020-07-19 15:33:51 +0300

    Update woodie.js

--- komutlar/woodie.js
+++ komutlar/woodie.js
@@ -4,17 +4,17 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
  
     const embed = new Discord.RichEmbed()
-        .setDescription("**WOODIE**")
+        .setDescription("**Keykubat**")
         .setImage("https://vignette.wikia.nocookie.net/dont-starve-game/images/2/23/Woodie.png/revision/latest/scale-to-width-down/350?cb=20140330212814")
         .setThumbnail("https://vignette.wikia.nocookie.net/dont-starve-game/images/2/23/Woodie.png/revision/latest/scale-to-width-down/350?cb=20140330212814")
         .setColor(0x00AE86)
-        .addField("Lakabı", "The Lumberjack (Oduncu)", true)
+        .addField("Adı", "Keykubat", true)
         .addField("Yetenekleri", `
-        *Çok güzel bir baltası var
-   *Korkunç bir sırrı var (Werebeaver)
+        *İnsanları tek görüşte ondan nefret etmelerini sağlayabilmek
+   *Şizofrenlik
    `, true)
-   .addField("Motto", `That's nice tree, eh? (güzel ağaç, ha?)`, true)
-   .addField("selam", "haha naber")
+   .addField("Motto", ``, true)
+   .addField("", "")
    
 
    message.channel.send(embed)
@@ -28,7 +28,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'woodie', 
-  description: 'Woodie the Lumberjack hakkında bilgi verir',
-  usage: 'woodie'
+  name: 'Keykubat', 
+  description: 'Keykubat Karakteri hakkında bilgi verir',
+  usage: 'Keykubat'
 };

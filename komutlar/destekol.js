1cf4ac7b3749e7932a8e47f2c8a0bf3d976f88a6
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2020-07-21 11:14:04 +0300

    Update and rename destekol to destekol.js

--- komutlar/destekol.js
+++ komutlar/destekol.js
@@ -5,6 +5,8 @@ exports.run = function(client, message) {
  
     const embed = new Discord.RichEmbed()
         .setDescription("**Skype**")
+        .setImage("https://i.pinimg.com/originals/ba/d2/93/bad293d77e09748f485665a25b631261.jpg")
+        .setThumbnail("https://images.discordapp.net/avatars/510114241307607051/b9f0bb37abe8613f965616d4666a6ca8.png?size=512")
         .setColor(0x00AE86)
         .addField("Adı", "Skype", true)
         .addField("Yetenekleri", `
@@ -12,9 +14,7 @@ exports.run = function(client, message) {
    *Sana yardım etmek
    `, true)
    .addField("Motto", `Skype kalbimizde yaşıyor`, true)
-   .addField("Eskiler bilir", "yeniler bilmez")
-   
-
+   .addField("İşte Ücretsiz Destek Linki", "https://top.gg/bot/510114241307607051/vote")
    message.channel.send(embed)
 };
 

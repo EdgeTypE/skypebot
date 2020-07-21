5b7cd586d80002f3746fc5907247afdc845b82f9
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2020-07-21 13:16:32 +0300

    Update balık-tut.js

--- komutlar/balık-tut.js
+++ komutlar/balık-tut.js
@@ -6,8 +6,7 @@ message.channel.send('Sanarım oltana balık düştü.').then(message => {
 var espriler = ['Sazan Tuttun! :fish:' ,'Köpek Balığı Tuttun.' ,'Uskumru Tuttun! :fish:' ,'Mezgit Tuttun! :fish:' ,'Japon Balığı Tuttun Yemeyi Düşünmüyorsun Herhalde?' ,'Hamsi Tuttun! :fish:' ,'Levrek Tuttun! :fish:' ,'Hiçbirşey Tutamadın Maalesef! :wastebasket:' ,'Alabalık Tuttun! :fish:' ,'Maalesef Balık Oltadan Kaçtı! :wastebasket:' ,'İstavrit Tuttun! :fish:'];
   var espri = espriler[Math.floor(Math.random() * espriler.length)];
         message.edit(`${espri}`);
-    }
- });
+};
 
 exports.conf = {
   enabled: true,

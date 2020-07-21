261b60f2d79436c030bc6542ceb6ac25e37be127
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2020-07-21 12:56:36 +0300

    Update ateş.js

--- komutlar/ateş.js
+++ komutlar/ateş.js
@@ -1,29 +1,42 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
-const snekfetch = require("snekfetch");
-   let guild = message.guild
-   let reason = args.slice(1).join(' ');
-   let user = message.mentions.users.first();
-   if (message.mentions.users.size < 1) return message.reply('Kime ateş edeceksen etiketlemelisin.').catch(console.error);
-   message.channel.send('Ateş ediliyor....')
-       .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
-       .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
-       .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
-       .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
-       .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
-       .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
-       .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
-       .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
-       .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
-       .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
-       .then(nmsg => nmsg.edit(`${Random[kafasınasık]}`));
-   var Random = [
-   'Reiz sen ne yaptın? Adam öldü.',
-   'Cazibene dayanamayıp adam kendini öldürdü.',
-   'Adam yoğun bakımda, Kaç.',
-       'Iskaladın tekrar dene.',
-   ];
-   var kafasınasık = Math.floor(Math.random() * Random.length);
+  const snekfetch = require("snekfetch");
+snekfetch.get(`https://discordbots.org/api/bots/${client.user.id}/check?userId=${message.author.id}`)
+.then(response => {
+var check = 1;
+if(check == 1) {  
+    let guild = message.guild
+    let reason = args.slice(1).join(' ');
+    let user = message.mentions.users.first();
+    if (message.mentions.users.size < 1) return message.reply('Kime ateş edeceksen etiketlemelisin.').catch(console.error);
+    message.channel.send('Ateş ediliyor....')
+        .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
+        .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
+        .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
+        .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
+        .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
+        .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
+        .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
+        .then(nmsg => nmsg.edit('https://goo.gl/fWHUqt'))
+        .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
+        .then(nmsg => nmsg.edit('https://goo.gl/91Y2az'))
+        .then(nmsg => nmsg.edit(`${Random[kafasınasık]}`));
+    var Random = [
+    'Reiz sen ne yaptın? Adam öldü.',
+    'Cazibene dayanamayıp adam kendini öldürdü.',
+    'Adam yoğun bakımda, Kaç.',
+        'Iskaladın tekrar dene.',
+    ];
+    var kafasınasık = Math.floor(Math.random() * Random.length);
+  } else {
+let embed = new Discord.RichEmbed()
+      .setTitle('HATA')
+      .setColor('RANDOM')
+      .setDescription(`${client.emojis.get(client.emojiler.hayır)} **Hata**, bu komutu kullanmak için **12 saat aralıkla** **[BURADAN](https://discordbots.org/bot/${client.user.id}/vote)**  botu oylamanız gerekmektedir. Onaylanması **1-4** dakikayı bulabilir, lütfen bekleyin. `)
+    message.channel.send(embed)
+      return }});
+  
+};
 
 exports.conf = {
     enabled: true,

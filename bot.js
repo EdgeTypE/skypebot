1fe103d612b7c4552e8de77448114d34257d54ec
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2020-11-19 11:27:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,8 +154,8 @@ client.elevation = message => {
   if(!message.guild) {
 	return; }
   let permlvl = 0;
-  if (message.guildMember.hasPermission("BAN_MEMBERS")) permlvl = 2;
-  if (message.guildMember.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if(msg.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if(msg.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
 };

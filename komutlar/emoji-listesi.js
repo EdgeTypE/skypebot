c3139a2ca57ed4326088e5e0106bef367d9788bf
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2020-12-24 10:22:35 +0300

    Create emoji-listesi.js

---
+++ komutlar/emoji-listesi.js
@@ -0,0 +1,22 @@
+const types = ['animated', 'regular'];
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+  if(!args[0]) return message.channel.send(":warning: Test");
+    const type = args[0].toLowerCase();
+		const emojis = message.guild.emojis.filter(emoji => type === 'animated' ? emoji.animated : !emoji.animated);
+		if (!emojis.size) return message.channel.send(`:warning: Bu sunucu ${type} emojilere sahip değil.`);
+    let emotelist = new Discord.RichEmbed()
+    .setAuthor("😃 Emote List", message.guild.displayAvatarURL)
+    .setColor("#4ea4ed")
+    .setDescription(emojis.map(emoji => emoji.toString()).join(' '), { split: { char: ' ' } });
+    return message.channel.send(emotelist)
+};
+exports.conf = {
+    aliases: ["emotes","emoji-listesi"]
+  };
+
+module.exports.help = {
+  name: "emoji-listesi"
+
+}

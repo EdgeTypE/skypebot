9413e8d0939b87a30746e90b0874ceb39010939e
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2020-12-30 17:31:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,6 @@ const chalk = require('chalk');
 const fs = require('fs');
 const moment = require('moment');
 
-const db = require("quick.db");
 const jimp = require("jimp");
 const Jimp = require("jimp");
 const snekfetch = require("snekfetch");

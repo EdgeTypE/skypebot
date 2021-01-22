0c93712be82eb170cee2a613830190c3989dbc1f
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2021-01-22 23:00:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,8 +82,8 @@ client.unload = command => {
 
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('**Aleyküm selam**');
+  if (msg.content.toLowerCase() === 'Esen Ola') {
+    msg.reply('**Esenlikler getirdin**');
   }
 });
 

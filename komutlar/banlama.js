7a6ceede01849e2af05e4618a033986da32365ab
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2022-06-09 23:40:28 +0300

    Delete banlama.js

--- komutlar/banlama.js
+++
@@ -1,9 +0,0 @@
-message.guild.channels.create('name', {
-	type: 'GUILD_TEXT',
-    permissionOverwrites: [{
-    	id: message.guild.id,
-        allow: ['VIEW_CHANNEL'],
-        deny: ['SEND_MESSAGES'],
-	}]
-});
-console.info(`name channel: mapus | type: text channel`)
